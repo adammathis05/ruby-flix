74e0d88aedeebf07da2de99de38cc9727ca8f89c
Adam Mathis <adam.mathis05@gmail.com>
2024-11-20 13:29:05 -0600

    adds functionality to add a new movie review to the UI

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,33 @@
 class ReviewsController < ApplicationController
 
+  before_action :set_movie
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
 
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @review = @movie.reviews.new(review_params)
+    if @review.save
+      redirect_to movie_reviews_path(@movie),
+        notice: "Your review has been successfully added!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+end
+
+private 
+
+def review_params
+  params.require(:review).permit(:name, :comment, :stars)
+end
+
+def set_movie
+  @movie = Movie.find(params[:movie_id])
 end
