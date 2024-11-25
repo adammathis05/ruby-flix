4b4a0b4aa49c4e3d2eca21eeadc3e589b8e58fc4
Adam Mathis <adam.mathis05@gmail.com>
2024-11-25 12:50:13 -0600

    adds user authentication on sign-in page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if 
+      user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}"
+    else
+      flash.now[:alert] = "Invalid username/password"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+
+  end
+
+end
