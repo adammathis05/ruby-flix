b0205b62dec0cb1d3fd37476b98626ab00059d8f
Adam Mathis <adam.mathis05@gmail.com>
2024-11-25 15:46:34 -0600

    updates authorization for all events and updates application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -23,29 +26,31 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
-
   end
 
   def update
-  @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to @user, notice: "Account successfully updated!"
+      redirect_to @user, notice: "Account successfully updated"
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
+    session[:user_id] = nil
       redirect_to movies_url, status: :see_other,
-      alert: "Account successfully deleted!"
+      alert: "Account successfully deleted"
   end
 
 end
 
 private
+
+def require_correct_user
+  @user = User.find(params[:id])
+  redirect_to root_url, status: :see_other unless current_user?(@user)
+end
 
 def user_params
   params.require(:user).
